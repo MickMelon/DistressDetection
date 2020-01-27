0d7d2d362252af1d7e6c81e76e98a9e459716018
MickMelon <michael.mcmillan@outlook.com>
2020-01-27 14:37:22 +0000

    Improve keyword spotting

--- src/keyword_spotting.py
+++ src/keyword_spotting.py
@@ -1,9 +1,12 @@
-keywords = ["background", "depth", "large", "stockings", "john", "keyword", "test"]
+keywords = ["background", "depth", "large", "stockings", "john", "keyword", "test", "sell"]
+
 
 def spot_keyword(text):
     words_spotted = 0
+    split_text = text.split()
+
     for i in range(len(keywords)):
-        if keywords[i] in text:
+        if keywords[i] in split_text:
             print("Spotted word " + keywords[i])
             words_spotted += 1
 
